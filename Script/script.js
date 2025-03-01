533e564be69ec38b7034cd9bd0e502c102052b4b
iftakhersifat <hossensifat615@gmail.com>
2025-03-02 02:11:26 +0600

    added history all parts

--- Script/script.js
+++ Script/script.js
@@ -4,6 +4,12 @@ let checkboxCount = 24;
 document.querySelectorAll("#Completed-btn").forEach(button => {
     button.addEventListener("click", function (event) {
         event.preventDefault(); 
+
+        // const taskName =document.getElementById("card-title").innerText;
+          const taskName = this.closest('.card').querySelector('h3').innerText;
+          const currentTime = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', second: '2-digit'});
+
+
         alert("Board updated Successfully"); 
         
         if (taskCount > 0) {
@@ -15,5 +21,14 @@ document.querySelectorAll("#Completed-btn").forEach(button => {
 
         // this.style.display = "none";
         this.disabled = true;
+
+        const activityLogContainer = document.querySelector(".activity-log");
+        const addDiv = document.createElement("div");
+        addDiv.classList.add("bg-[#F4F7FF]", "p-4", "mt-4", "mb-4", "rounded-xl");
+        addDiv.innerHTML = `You have completed The Task <strong>${taskName}</strong> at ${currentTime}`;
+        activityLogContainer.appendChild(addDiv);
+        
+
+
     });
 });
